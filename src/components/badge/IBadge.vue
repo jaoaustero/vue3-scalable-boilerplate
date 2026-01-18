4094aabe3ea6468d6d553794ca319e18b642f64b
Jao Austero <jerald.austero0229@gmail.com>
2026-01-18 12:12:06 +0800

    Add badge component, style, story and unit test

---
+++ src/components/badge/IBadge.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = withDefaults(
+	defineProps<{
+		count: number;
+	}>(),
+	{
+		count : 0,
+	},
+);
+</script>
+
+
+<template>
+	<div class="i-badge">
+		{{ props.count <= 99 ? props.count : '99+' }}
+	</div>
+</template>
+
+<style lang="scss">
+@use "./IBadge.scss" as *;
+</style>
